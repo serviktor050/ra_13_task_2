0cfe69294a86d9bd806346c32f1a531aeb4837ab
serviktor050 <mr.s-gagulin@yandex.ru>
2021-04-16 14:06:42 +0300

    Реализован выбор услуги

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -2,12 +2,27 @@ import { takeLatest, put, spawn, call } from "redux-saga/effects";
 import {
   fetchServicesFailure,
   fetchServicesSuccess,
+  serviceChooseFailure,
+  serviceChooseSuccess,
 } from "../actions/actionCreators";
-import { FETCH_SERVICES_REQUEST } from "../actions/actionTypes";
+import {
+  FETCH_SERVICES_REQUEST,
+  SERVICE_CHOOSE_REQUEST,
+} from "../actions/actionTypes";
 
 export const fetchServices = () => async () => {
   const response = await fetch(
     "https://ra-13-task-2-server.herokuapp.com/api/services"
+  );
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  }
+  return await response.json();
+};
+
+export const chooseService = (id) => async () => {
+  const response = await fetch(
+    `https://ra-11-task-1-server.herokuapp.com/api/services/${id}`
   );
   if (!response.ok) {
     throw new Error(response.statusText);
@@ -25,12 +40,28 @@ function* handleFetchServicesRequestSaga() {
   }
 }
 
+// worker
+function* handleChooseServiceRequestSaga(action) {
+  try {
+    const data = yield call(chooseService(action.payload.id));
+    yield put(serviceChooseSuccess(data));
+  } catch (e) {
+    yield put(serviceChooseFailure(e.message));
+  }
+}
+
 // watcher
 function* watchFetchServicesSaga() {
   yield takeLatest(FETCH_SERVICES_REQUEST, handleFetchServicesRequestSaga);
 }
 
+// watcher
+function* watchChooseServiceSaga() {
+  yield takeLatest(SERVICE_CHOOSE_REQUEST, handleChooseServiceRequestSaga);
+}
+
 //Корневая Saga
 export default function* saga() {
   yield spawn(watchFetchServicesSaga);
+  yield spawn(watchChooseServiceSaga);
 }
